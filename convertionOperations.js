b01b75f81de739316b76218029fad2452c1c0444
Pushpendra Yadav <pushpydv.s02@gmail.com>
2025-08-27 18:08:03 +0530

    convertionOperations

---
+++ convertionOperations.js
@@ -0,0 +1,58 @@
+//let score = 33
+
+/*console.log(typeof score);
+console.log(typeof(score)); // output will be Number */
+
+/*let score ="33"
+console.log(typeof(score)); // output will be string 
+
+/*let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // output will be Number
+console.log(valueInNumber) // output will be 33  */
+
+/*let score ="33abc" 
+console.log(typeof score) // output will be string 
+console.log(score) // output will be 33abc
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)  // output will be Number
+console.log(valueInNumber) // output will be NaN (Not a Number)*/
+
+/*let score = null
+console.log(score) // output will be null
+let valueInNumber = Number(score)
+console.log(valueInNumber) // output will be 0*/
+
+/*let score = undefined
+console.log(score) // output will be undefinde
+let valueInNumber = Number(score)
+console.log(valueInNumber) // output will be NaN*/
+
+/*let score = true
+console.log(score) // output will be true
+let valueInNumber = Number(score)
+console.log(valueInNumber) // output will be 1*/
+
+let score = "PUSHP"
+console.log(score) // output will be PUSHP
+let valueInNumber = Number(score)
+console.log(valueInNumber) // output will be NaN*/
+
+// "33" => 33
+//"33abc => NaN"
+// true => 1 & false =>0
+
+let isloggedIn =1
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn) // output will be true 
+
+// 1 => true && 0 => false
+// "" => false
+// "Pushp" => true 
+
+let someNumber = 33
+let someString = String(someNumber)
+console.log(someString) // output will be 33
+console.log(typeof(someString)) // output will be string 
+
+
