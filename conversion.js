beb8b2d01f2b10c0319e40cade222362411759a0
Pushpendra Yadav <pushpydv.s02@gmail.com>
2025-08-28 17:39:49 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,21 @@
+/*console.log( 2 > 1);
+console.log( 2 >= 1);
+console.log( 2 < 1);
+console.log( 2 == 1);
+console.log( 2 != 1);*/
+
+//console.log("2" > 1); // here it convert the string into number and then it comapre it so the output will be true.
+//console.log(null > 0); //false
+//console.log(null == 0); //false
+/*console.log(null >= 0); /*true (The reason is that an equality check == and comparisons >, <, >=, <= work differently.
+                            Comparisons convert null to a number, treating it as 0.
+                            that's why null >= 0 is true and null > 0 is false */
+
+/*console.log(undefined = 0);
+console.log(undefined == 0); // false
+console.log(undefined >= 0); //false
+console.log(undefined <= 0); //false */
+
+//console.log("2" === 2) // false.
+                        // it check datatypr first and the give the answer.
+
