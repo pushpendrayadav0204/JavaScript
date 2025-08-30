2b4dea5adf329c4052233788950afe5543ecc821
Pushpendra Yadav <pushpydv.s02@gmail.com>
2025-08-30 14:06:10 +0530

    String

---
+++ String.js
@@ -0,0 +1,32 @@
+/*const name  ="PUSHP";
+const repoCount = 3;
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Pushpendra')
+console.log(gameName [0]); // output will be "P".
+console.log(gameName[5]); // output will be "e".
+console.log(gameName.length) // it provide the length of string.
+console.log(gameName.toUpperCase()); //PUSHPENDRA
+console.log(gameName.charAt(2)); // output will be "s".
+console.log(gameName.indexOf('h')); //output will be "3".
+
+const newString = gameName.substring(0,4);
+console.log(newString); // output will be "Push".*/
+/*const gameName = new String('Pushpendra');
+const anotherString = gameName.slice(-2, -1);
+console.log(anotherString); //// output will be "r"
+
+const newStringOne = "   PUSHP  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://pushp.com/pushp%20yadav";
+console.log(url.replace('%20', '_')); // Output will be "https://pushp.com/pushp_yadav".
+console.log(url.includes('pushp')); // true.
+console.log(url.includes('pushpendra')); //false.*/
+
+const string = 'Pushp_endra';
+console.log(string.split('_')); // output will be "[ 'Pushp', 'endra' ]".
