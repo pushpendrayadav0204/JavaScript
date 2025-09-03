b29bd8fc9dce10c767540eac8013b35a064069f7
Pushpendra Yadav <pushpydv.s02@gmail.com>
2025-09-03 09:27:44 +0530

    Functions with object and Array

--- functions.js
+++ functions.js
@@ -43,7 +43,7 @@ console.log("Result: ",result); // Output => 11.*/
 const result = addTwoNumbers(3,8);
 console.log("Result: ",result); // Output => PUSHP , Result:  11.*/
 
-function addTwoNumbers(number1, number2){
+/*function addTwoNumbers(number1, number2){
     return number1 + number2;
 }
 const result = addTwoNumbers(3,1); 
@@ -54,4 +54,43 @@ function loginUserMessage(username){
     return `${username} just logged in`;
 }
 console.log(loginUserMessage("Pushp")); //Output => Pushp just logged in.
-console.log(loginUserMessage()); //Output => undefined just logged in.
+console.log(loginUserMessage()); //Output => undefined just logged in.*/
+
+
+function calculateCartPrice(...num1){  //"..." rest operator
+      return num1
+}
+//console.log(calculateCartPrice(200, 400, 500)) // Output => [ 200, 400, 500 ].
+
+function calculateCartPrice(value1, value2, ...num1){
+    return num1;
+}
+//console.log(calculateCartPrice(100,200,300,400,500,600)); // Output => [ 300, 400, 500, 600 ].
+
+
+const user = {
+    username: "Pushpendra",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`); // Output => username is Pushpendra and price is 199
+}
+handleObject(user);
+
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`); // Output => username is PUSHP and price is 399.
+}
+handleObject({
+    username: "PUSHP",
+    price: 399
+});
+
+
+const myNewArray = [200, 400, 500, 600]
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+//console.log(returnSecondValue(myNewArray)); // Output => 400.
+console.log(returnSecondValue([200,800,500,140,502])); // Output => 800.
